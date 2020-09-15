a52c2f8fdbc2a7e7809c4336106881dcee93e72b
旭 王 <wangxu@outlook.jp>
2020-09-15 16:56:46 +0800

    添加项目文件。

---
+++ GDI和GDI Plus/MemDC.cpp
@@ -0,0 +1,22 @@
+﻿#include "stdafx.h"
+#include "MemDC.h"
+
+
+CMemDC::CMemDC(HDC hdc, const RECT& rect)
+{
+	m_hdc = hdc;
+	m_hMemDc = CreateCompatibleDC(hdc);
+	m_rect = rect;
+	m_hBitmap = CreateCompatibleBitmap(hdc, rect.right - rect.left, rect.bottom - rect.top);
+	m_hOldBitmap = (HBITMAP)SelectObject(m_hMemDc, m_hBitmap);
+}
+
+
+CMemDC::~CMemDC()
+{
+	BitBlt(m_hdc, m_rect.left, m_rect.top, m_rect.right - m_rect.left, m_rect.bottom - m_rect.top,
+		m_hMemDc, 0, 0, SRCCOPY);
+	SelectObject(m_hMemDc, m_hOldBitmap);
+	DeleteObject(m_hBitmap);
+	DeleteDC(m_hMemDc);
+}
---
+++ GDI和GDI Plus/MemDC.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+class CMemDC
+{
+public:
+	CMemDC(HDC hdc, const RECT& rect);
+	virtual ~CMemDC();
+
+	operator HDC()
+	{
+		return m_hMemDc;
+	}
+private:
+	HDC m_hdc, m_hMemDc;
+	RECT m_rect;
+	HBITMAP m_hBitmap, m_hOldBitmap;
+};
